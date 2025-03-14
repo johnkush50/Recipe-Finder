be2d5cfd93316f73afa362b54a7d6fd27800f875
John Kushwarraz <jfk6111@psu.edu>
2025-03-13 20:52:07 -0400

    Add Github Actions workflow for source deployement

---
+++ config.example.js
@@ -0,0 +1,8 @@
+// config.example.js
+// Copy this file to a new file named config.js and replace 'YOUR_API_KEY' with your actual API key.
+const CONFIG = {
+  API_KEY: 'YOUR_API_KEY'
+};
+
+// If using modules, you can export it:
+// export default CONFIG;
